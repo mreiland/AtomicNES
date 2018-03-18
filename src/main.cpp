b1c084b8fd37f1e4655e046faa624b4b23b0c61f
Michael Reiland <dev@michaelreiland.net>
2018-03-18 12:05:29 -0500

    stubbed out execution loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,31 @@
 #include "fmt/format.h"
 #include "fmt/printf.h"
 
+#include "instructions.h"
+
+#include "cpu.h"
+#include "executor.h"
+#include "memory.h"
+
 int main() {
-    std::cout << "Hello, world!" << std::endl;
-    std::cout << "Hello, world!" << std::endl;
-    
-    std::string s = fmt::format("{0}{1}{0}", "abra", "cad");
-    std::cout << s << std::endl;
-    
-    fmt::print("Hello, {}!\n", "world");
-    fmt::printf("Hello, {}!", "world");
-    
-    // appears to be necessary to get fmt::print and fmt::printf to output.
-    // std::cout.flush() isn't enough for some odd reason.  We need to either
-    // end the above with a \n or do an explicit std::endl;  I'm unsure why
-    // std::cout.flush() isn't enough, but there you go...
-    // 
-    std::cout << std::endl; 
-    return 0;
+  instructions::initialize();
+  
+  // fetch ROM from HD
+  
+  cpu cpu;
+  decode_info decoded;
+  memory mem;
+  
+  executor::power_on(&cpu, &mem);
+  
+  while(true) {
+    decoded = executor::fetch_and_decode(&cpu, &mem);
+    //log_info(decode_info);
+    executor::execute(&cpu, &mem, decoded);
+  }
+  
+  // ensure flush
+  std::cout << std::endl;
+  return 0;
 }
 
