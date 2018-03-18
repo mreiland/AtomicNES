1d0dca5a771c836d7e4de574f77d6843f5520ab0
Michael Reiland <dev@michaelreiland.net>
2018-03-18 12:03:57 -0500

    stubbed out memory datastructure

---
+++ src/memory.cpp
@@ -0,0 +1,10 @@
+#include "memory.h"
+
+
+uint8_t memory::get() {
+  return 0;
+}
+
+uint8_t memory::get16() {
+  return 0;
+}
---
+++ src/memory.h
@@ -0,0 +1,15 @@
+#ifndef ATOM_NES_MEMORY_H
+#define ATOM_NES_MEMORY_H
+
+#include <array>
+
+class memory {
+public:
+  uint8_t get();
+  uint8_t get16();
+
+private:
+  //std::array<uint8_t, 65535> mem;
+};
+
+#endif
