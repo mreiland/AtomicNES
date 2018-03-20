c6a6808a46ecd94446fbfefaa64469eb6f1af6ee
Michael Reiland <dev@michaelreiland.net>
2018-03-20 05:42:33 -0500

    implemented get/set flags functions

--- src/Cpu.h
+++ src/Cpu.h
@@ -4,12 +4,48 @@
 #include <cstdint>
 
 struct Cpu {
+  // registers
   uint8_t a;
   uint8_t x;
   uint8_t y;
-  
-  uint16_t SP;
-  uint8_t p;
+
+  uint16_t PC;
+  uint8_t SP;
+
+  // processor flags
+  bool C;
+  bool Z;
+  bool I;
+  bool D;
+  bool B;
+  bool U;
+  bool V;
+  bool N;
+
+  inline void set_flags(uint8_t flags) {
+    C = ((flags >> 0) & 1) > 0;
+    Z = ((flags >> 1) & 1) > 0;
+    I = ((flags >> 2) & 1) > 0;
+    D = ((flags >> 3) & 1) > 0;
+    B = ((flags >> 4) & 1) > 0;
+    U = ((flags >> 5) & 1) > 0;
+    V = ((flags >> 6) & 1) > 0;
+    N = ((flags >> 7) & 1) > 0;
+  }
+
+  inline uint8_t get_flags() {
+    uint8_t flags;
+     flags = flags + (C << 0);
+     flags = flags + (Z << 1);
+     flags = flags + (I << 2);
+     flags = flags + (D << 3);
+     flags = flags + (B << 4);
+     flags = flags + (U << 5);
+     flags = flags + (V << 6);
+     flags = flags + (N << 7);
+
+    return flags;
+  }
 };
 
 #endif
