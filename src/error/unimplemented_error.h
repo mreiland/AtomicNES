d4a2dab2f63725deb72aed0be24b0193d0b52aa9
Michael Reiland <dev@michaelreiland.net>
2018-03-22 05:24:55 -0500

    added error namespace

--- src/error/unimplemented_error.h
+++ src/error/unimplemented_error.h
@@ -3,11 +3,13 @@
 
 #include <stdexcept>
 
-class unimplemented_error : public std::logic_error {
-  public:
-    unimplemented_error(const std::string &msg) : std::logic_error(msg) { }
-    ~unimplemented_error() {}
-};
+namespace error {
+  class unimplemented_error : public std::logic_error {
+    public:
+      unimplemented_error(const std::string &msg) : std::logic_error(msg) { }
+      ~unimplemented_error() {}
+  };
+}
 
 
 #endif
