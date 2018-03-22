fbfc43dc5212976c58eafa3c225d82e0634bbe4b
Michael Reiland <dev@michaelreiland.net>
2018-03-21 19:02:29 -0500

    implemented cpu status flags tests

--- tests/Cpu.cpp
+++ tests/Cpu.cpp
@@ -1,6 +1,7 @@
 #include "doctest.h"
 
 #include "Cpu.h"
+#include "fmt/format.h"
 
 #include <limits>
 #include <cstdint>
@@ -11,12 +12,92 @@ TEST_CASE("Check packing/unpacking of processor flags") {
 
   Cpu cpu;
 
+  uint8_t flags = 0;
+
+  // NOTE: bit 5 is always 0, so we explicitly set it as such before testing
   for(auto i = 0; i < std::numeric_limits<uint8_t>::max();++i) {
     cpu.set_flags(i);
-    auto flags = cpu.get_flags();
+    flags = cpu.get_flags();
+    auto expected = i & 0xEF;
 
-    REQUIRE(flags == i);
+    auto msg = fmt::format("cpu status flag test failed for value '{}'",i);
+    REQUIRE_MESSAGE(flags == expected, msg);
   }
+  
+
+  // bit 0
+  cpu.set_flags(0);
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+  cpu.C = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 1);
+  cpu.C = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 1
+  cpu.set_flags(0);
+  cpu.Z = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 2);
+  cpu.Z = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 2
+  cpu.set_flags(0);
+  cpu.I = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 4);
+  cpu.I = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 3
+  cpu.set_flags(0);
+  cpu.D = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 8);
+  cpu.D = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 4
+  cpu.set_flags(0);
+  cpu.B = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+  cpu.B = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 5
+  cpu.set_flags(0);
+  cpu.U = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 32);
+  cpu.U = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 6
+  cpu.set_flags(0);
+  cpu.V = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 64);
+  cpu.V = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
+
+  //bit 7
+  cpu.set_flags(0);
+  cpu.N = true;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 128);
+  cpu.N = false;
+  flags = cpu.get_flags();
+  REQUIRE(flags == 0);
 }
 
 
