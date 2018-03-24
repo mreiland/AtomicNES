7502b0b50b6ec42da3cf95bfe59c6212d368e8cd
Michael Reiland <dev@michaelreiland.net>
2018-03-24 09:07:21 -0500

    added debugger and we now catch const char * exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include "Instruction.h"
 
+#include "Debugger.h"
 #include "Cpu.h"
 #include "Executor.h" 
 #include "Memory.h"
@@ -27,17 +28,24 @@ int main() {
   //fmt::print("|0x{:>04X}|\n",cpu.PC);
 
   logger::NesTest log("resources/nestest-out.log");
+  Debugger dbg;
 
   try {
+    dbg.set_pc_breakpoint(0xC7F8);
+
     while(true) {
       Executor::fetch_and_decode(cpu, mem, &decoded);
       log.log(cpu, mem, decoded);
+      dbg.debug(&cpu, &mem, decoded);
 
       Executor::execute(&cpu, &mem, decoded);
     }
   }
   catch(std::exception &e) {
     std::cout << "Exception thrown: " << e.what() << std::endl;
+  }
+  catch(const char*e) {
+    std::cout << "Exception thrown: " << e << std::endl;
   }
   
   // ensure flush
