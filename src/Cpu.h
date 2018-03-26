3c6183d31076714e4bf7adc98a876bdcae08c875
Michael Reiland <dev@michaelreiland.net>
2018-03-26 10:02:02 -0500

    get/set flags in cpu no longer forces bit 5

--- src/Cpu.h
+++ src/Cpu.h
@@ -29,7 +29,7 @@ struct Cpu {
     Z = ((flags >> 1) & 1) > 0;
     I = ((flags >> 2) & 1) > 0;
     D = ((flags >> 3) & 1) > 0;
-    //B = ((flags >> 4) & 1) > 0;
+    B = ((flags >> 4) & 1) > 0;
     U = ((flags >> 5) & 1) > 0;
     V = ((flags >> 6) & 1) > 0;
     N = ((flags >> 7) & 1) > 0;
@@ -41,7 +41,7 @@ struct Cpu {
     flags = flags + (Z << 1);
     flags = flags + (I << 2);
     flags = flags + (D << 3);
-    //flags = flags + (B << 4);
+    flags = flags + (B << 4);
     flags = flags + (U << 5);
     flags = flags + (V << 6);
     flags = flags + (N << 7);
