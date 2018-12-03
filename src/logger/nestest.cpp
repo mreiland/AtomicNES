c27b975b726ea799e8288084642527804d60c323
Michael Reiland <Michael.Reiland@gmail.com>
2018-12-03 05:07:39 -0600

    added logging for ABS addressing mode

--- src/logger/nestest.cpp
+++ src/logger/nestest.cpp
@@ -51,9 +51,18 @@ void NesTest::log(const Cpu &cpu, const Memory &mem, const DecodeInfo &decoded) 
       break;
     case AddressingMode::ZPX:   throw error::unimplemented_error("ZeroPage,X addressing mode is not implemented IN THE LOGGER"); break;
     case AddressingMode::ZPY:   throw error::unimplemented_error("ZeroPage,Y addressing mode is not implemented IN THE LOGGER"); break;
-    case AddressingMode::ABS:   
+    case AddressingMode::ABS: {
       str = fmt::format("${:>02X}{:>02X} ", mem.read8(cpu.PC+2), mem.read8(cpu.PC+1));
+
+      // I'm unsure why these instructions output differently even though they're ABS addressing mode
+      //
+      auto opcode = decoded.instruction->opcode;
+      if(opcode == 0x8E || opcode == 0xAE || opcode == 0xAD) {
+        str += fmt::format("= {:>02X} ",decoded.val1);
+      }
+      // 0x8E seems to be a 1 off in that it's ABS, but has = 00 after it in the logs.  investigate
       break;
+    }
     case AddressingMode::ABSX:  throw error::unimplemented_error("Absolute,X addressing mode is not implemented IN THE LOGGER"); break;
     case AddressingMode::ABSY:  throw error::unimplemented_error("Absolute,Y  addressing mode is not implemented IN THE LOGGER"); break;
     case AddressingMode::IND:   throw error::unimplemented_error("Indirect addressing mode is not implemented IN THE LOGGER"); break;
